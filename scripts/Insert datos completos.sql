eecb6c82dd54e0827bd3b5d99a02a3b0fdb448b8
Guillermo ENH <guillermoenh@gmail.com>
2021-09-14 11:48:55 -0500

    Procedure consulta

---
+++ scripts/Insert datos completos.sql
@@ -0,0 +1,36 @@
+insert into Vendedores values('VWEM01', 'DE ALBA VILLALOBOS LILIANA LIZBETH');
+insert into Vendedores values('VWEM02', 'FACIO ESPINOSA JUAN CARLOS');
+insert into Vendedores values('VWEM03', 'GOMEZ CANO JOSE MIGUEL');
+insert into Vendedores values('VWEM04', 'GOMEZ CASTAÑEDA MARIA FERNANDA');
+insert into Vendedores values('VWEM05', 'GOMEZ LOPEZ RAFAEL');
+
+insert into unidadesvendidas values('VWCAR2020GOLST', 'GOL 2020');
+insert into unidadesvendidas values('VWCAR2020POLOAUT', 'POLO 2020');
+insert into unidadesvendidas values('VWCAR2020VENTOST', 'VENTO 2020');
+insert into unidadesvendidas values('VWCAR2020JETTAGLI', 'JETTA 2020');
+
+
+insert into mes values(1, 20, 12, 11, 'VWCAR2020GOLST', 'VWEM01');
+insert into mes values(2, 7, 10, 5, 'VWCAR2020POLOAUT', 'VWEM01');
+insert into mes values(3, 6, 12, 12, 'VWCAR2020VENTOST', 'VWEM01');
+insert into mes values(4, 12, 10, 6, 'VWCAR2020JETTAGLI', 'VWEM01');
+
+insert into mes values(5, 12, 11, 5, 'VWCAR2020GOLST', 'VWEM02');
+insert into mes values(6, 10, 6, 10, 'VWCAR2020POLOAUT', 'VWEM02');
+insert into mes values(7, 7, 11, 11, 'VWCAR2020VENTOST', 'VWEM02');
+insert into mes values(8, 11, 6, 8, 'VWCAR2020JETTAGLI', 'VWEM02');
+
+insert into mes values(9, 11, 10, 10, 'VWCAR2020GOLST', 'VWEM03');
+insert into mes values(10, 10, 12, 6, 'VWCAR2020POLOAUT', 'VWEM03');
+insert into mes values(11, 10, 10, 10, 'VWCAR2020VENTOST', 'VWEM03');
+insert into mes values(12, 7, 12, 12, 'VWCAR2020JETTAGLI', 'VWEM03');
+
+insert into mes values(13, 10, 10, 6, 'VWCAR2020GOLST', 'VWEM04');
+insert into mes values(14, 6, 11, 12, 'VWCAR2020POLOAUT', 'VWEM04');
+insert into mes values(15, 6, 7, 10, 'VWCAR2020VENTOST', 'VWEM04');
+insert into mes values(16, 10, 11, 11, 'VWCAR2020JETTAGLI', 'VWEM04');
+
+insert into mes values(17, 9, 5, 7, 'VWCAR2020GOLST', 'VWEM05');
+insert into mes values(18, 0, 10, 11, 'VWCAR2020POLOAUT', 'VWEM05');
+insert into mes values(19, 8, 8, 6, 'VWCAR2020VENTOST', 'VWEM05');
+insert into mes values(20, 5, 10, 10, 'VWCAR2020JETTAGLI', 'VWEM05');
