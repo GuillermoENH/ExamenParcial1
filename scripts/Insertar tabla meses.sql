f68bf93fe09e7a42c68a7f8a44a9e920fd106f67
Guillermo ENH <guillermoenh@gmail.com>
2021-09-14 11:25:46 -0500

    Insercion Datos Mes

---
+++ scripts/Insertar tabla meses.sql
@@ -0,0 +1,23 @@
+insert into mes values (null,20,12,11,'VWCAR2020GOLST','01');
+insert into mes values (null,12,11,5,'VWCAR2020GOLST','02');
+insert into mes values (null,11,10,10,'VWCAR2020GOLST','03');
+insert into mes values (null,10,10,6,'VWCAR2020GOLST','04');
+insert into mes values (null,9,5,7,'VWCAR2020GOLST','05');
+
+insert into trimeste values (null,7,10,5,'VWCAR2020POLOAUT','VWEM01');
+insert into trimeste values (null,10,6,10,'VWCAR2020POLOAUT','VWEM02');
+insert into trimeste values (null,10,12,6,'VWCAR2020POLOAUT','VWEM03');
+insert into trimeste values (null,6,11,12,'VWCAR2020POLOAUT','VWEM04');
+insert into trimeste values (null,0,10,11,'VWCAR2020POLOAUT','VWEM05');
+
+insert into trimeste values (null,6,12,12,'VWCAR2020VENTOST','VWEM01');
+insert into trimeste values (null,7,11,11,'VWCAR2020VENTOST','VWEM02');
+insert into trimeste values (null,10,10,10,'VWCAR2020VENTOST','VWEM03');
+insert into trimeste values (null,6,7,10,'VWCAR2020VENTOST','VWEM04');
+insert into trimeste values (null,8,8,6,'VWCAR2020VENTOST','VWEM05');
+
+insert into trimeste values (null,12,10,6,'VWCAR2020JETTAGLI','VWEM01');
+insert into trimeste values (null,11,6,8,'VWCAR2020JETTAGLI','VWEM02');
+insert into trimeste values (null,7,12,12,'VWCAR2020JETTAGLI','VWEM03');
+insert into trimeste values (null,10,11,12,'VWCAR2020JETTAGLI','VWEM04');
+insert into trimeste values (null,5,10,10,'VWCAR2020JETTAGLI','VWEM05');
