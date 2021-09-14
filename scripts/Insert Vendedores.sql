03505870db614d5c3e31bfc4348a88e1955c3036
Guillermo ENH <guillermoenh@gmail.com>
2021-09-14 10:50:41 -0500

    Insertar Vendedores

---
+++ scripts/Insert Vendedores.sql
@@ -0,0 +1,5 @@
+insert into vendedores values('01','DE ALBA VILLALOBOS LILIANA LIZBETH');
+insert into vendedores values('02','FACIO ESPINOSA JUAN CARLOS');
+insert into vendedores values('03','GOMEZ CANO JOSE MIGUEL');
+insert into vendedores values('04','GOMEZ CASTAÑEDA MARIA FERNANDA');
+insert into vendedores values('05','GOMEZ LOPEZ RAFAEL');
